e0832ed9636c65c3b7507c5935cccabcdc0cd59c
UBeagle <80141258+UBeagle@users.noreply.github.com>
2021-07-27 00:12:22 -0700

    Initial commit

---
+++ src/main/java/me/beagle/toitravel/TOITravel.java
@@ -0,0 +1,17 @@
+package me.beagle.toitravel;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TOITravel extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
